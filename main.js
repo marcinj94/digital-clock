c03d563823f8f9253866ab5c0efcc6ec8691b255
beardofdoom <marcin.jusiak@gmail.com>
2019-01-07 11:41:51 +0100

    Final version of digital clock
    Changes in project:
    	- added js file
    	- changed font
    	- added media queries to css file

---
+++ main.js
@@ -0,0 +1,33 @@
+const showTime = document.querySelector('.time');
+const showDate = document.querySelector('.date');
+
+const daysName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+
+const showDateAndTime = () => {
+    const time = new Date();
+
+    let seconds = time.getSeconds();
+    let minutes = time.getMinutes();
+    let hours = time.getHours();
+
+    let day = time.getDate();
+    let month = time.getMonth() + 1;
+    let year = time.getFullYear();
+    let dayName = daysName[time.getDay()];
+
+    //Time
+    seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    hours = hours < 10 ? `0${hours}` : `${hours}`;
+
+    //Date
+    day = day < 10 ? `0${day}` : `${day}`;
+    month = month < 10 ? `0${month}` : `${month}`;
+
+    showTime.textContent = `${hours}:${minutes}:${seconds}`;
+
+    showDate.textContent = `${day}-${month}-${year} ${dayName}`
+}
+
+setInterval(showDateAndTime, 1000);
